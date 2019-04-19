fc1d8ce1af0c863a1113d011bd6b856a2601fad5
Solarex <rh.hou.work@gmail.com>
2019-04-19 17:43:21 +0800

    引入circle progress

---
+++ app/src/main/java/org/solarex/customviewdemos/ui/CircleProgressActivity.java
@@ -0,0 +1,14 @@
+package org.solarex.customviewdemos.ui;
+
+import org.solarex.customviewdemos.R;
+
+/**
+ * Created by houruhou on 09/04/2017.
+ */
+
+public class CircleProgressActivity extends BaseCustomViewActivity {
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_circleprogressbar;
+    }
+}
