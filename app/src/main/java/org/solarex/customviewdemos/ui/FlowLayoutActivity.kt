16b32bc19feb2cab92ea2799a91456951faf690e
Solarex <rh.hou.work@gmail.com>
2019-04-19 18:09:08 +0800

    引入flowlayout

---
+++ app/src/main/java/org/solarex/customviewdemos/ui/FlowLayoutActivity.kt
@@ -0,0 +1,17 @@
+package org.solarex.customviewdemos.ui
+
+import org.solarex.customviewdemos.R
+
+/**
+ * <pre>
+ *    Author: houruhou
+ *    CreatAt: 18:05/2019/4/19
+ *    Desc:
+ * </pre>
+ */
+ 
+class FlowLayoutActivity : BaseCustomViewActivity() {
+    override fun getLayoutId(): Int {
+        return R.layout.activity_flowlayout
+    }
+}
