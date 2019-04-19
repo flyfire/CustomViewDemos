9f016a2eec19a56ca52ca0e9c5dccd0703e99105
Solarex <rh.hou.work@gmail.com>
2019-04-19 17:57:14 +0800

    引入disscrollview

---
+++ app/src/main/java/org/solarex/customviewdemos/ui/DisscrollViewActivity.kt
@@ -0,0 +1,17 @@
+package org.solarex.customviewdemos.ui
+
+import org.solarex.customviewdemos.R
+
+/**
+ * <pre>
+ *    Author: houruhou
+ *    CreatAt: 17:51/2019/4/19
+ *    Desc:
+ * </pre>
+ */
+
+class DisscrollViewActivity : BaseCustomViewActivity() {
+    override fun getLayoutId(): Int {
+        return R.layout.activity_disscrollview
+    }
+}
