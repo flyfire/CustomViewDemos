c98d221213a5cd607a24930f92efd34b73e577a9
Solarex <rh.hou.work@gmail.com>
2019-04-21 11:40:35 +0800

    引入progress

---
+++ app/src/main/java/org/solarex/customviewdemos/ui/SolarexProgressActivity.kt
@@ -0,0 +1,17 @@
+package org.solarex.customviewdemos.ui
+
+import org.solarex.customviewdemos.R
+
+/**
+ * <pre>
+ *    Author: houruhou
+ *    CreatAt: 11:37/2019/4/21
+ *    Desc:
+ * </pre>
+ */
+ 
+class SolarexProgressActivity : BaseCustomViewActivity() {
+    override fun getLayoutId(): Int {
+        return R.layout.activity_solarexprogress
+    }
+}
