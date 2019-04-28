63cda8a5d4337053c7efa75d07fd747233ded2bd
Solarex <rh.hou.work@gmail.com>
2019-04-28 19:34:39 +0800

    weekly calendar

--- app/src/main/java/org/solarex/customviewdemos/ui/WeeklyCalendarActivity.kt
+++ app/src/main/java/org/solarex/customviewdemos/ui/WeeklyCalendarActivity.kt
@@ -1,6 +1,9 @@
 package org.solarex.customviewdemos.ui
 
 import android.os.Bundle
+import android.util.Log
+import com.solarexsoft.weeklycalendarview.WeeklyCalendarView
+import com.solarexsoft.weeklycalendarview.WeeklyItemModel
 import org.solarex.customviewdemos.R
 
 /**
@@ -12,11 +15,21 @@ import org.solarex.customviewdemos.R
  */
  
 class WeeklyCalendarActivity : BaseCustomViewActivity() {
+    companion object {
+        val TAG = WeeklyCalendarActivity.javaClass.name
+    }
     override fun getLayoutId(): Int {
         return R.layout.activity_weeklycalendar
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val wlc = findViewById<WeeklyCalendarView>(R.id.wcl_main)
+        wlc.setWeekAgo(30)
+        wlc.setOnWeeklyItemClickListener(object : WeeklyCalendarView.OnWeeklyItemClickListener{
+            override fun onItemClick(position: Int, model: WeeklyItemModel?) {
+                Log.d(TAG, "clicked -> " + model)
+            }
+        })
     }
 }
