745a6a3fbf0e1f604e67aca7129e437d67a0e62b
Solarex <rh.hou.work@gmail.com>
2019-06-19 11:34:59 +0800

    test html

---
+++ app/src/main/java/org/solarex/customviewdemos/ui/DemoTestActivity.kt
@@ -0,0 +1,31 @@
+package org.solarex.customviewdemos.ui
+
+import android.os.Bundle
+import android.text.Html
+import android.util.Log
+import android.widget.TextView
+import org.solarex.customviewdemos.R
+
+/**
+ * <pre>
+ *    Author: houruhou
+ *    CreatAt: 11:26/2019/6/19
+ *    Desc:
+ * </pre>
+ */
+ 
+class DemoTestActivity : BaseCustomViewActivity() {
+    val TAG = DemoTestActivity::class.java.simpleName
+
+    override fun getLayoutId(): Int {
+        return R.layout.activity_demo_test;
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val tv_content = findViewById<TextView>(R.id.tv_content)
+        val str = String.format(resources.getString(R.string.content_format), resources.getString(R.string.content))
+        Log.d(TAG, "str = $str")
+        tv_content.text = Html.fromHtml(str)
+    }
+}
