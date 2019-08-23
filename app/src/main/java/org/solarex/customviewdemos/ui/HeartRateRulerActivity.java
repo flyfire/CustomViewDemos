572962b8049298c7b858972b3d819fa9d0691018
Solarex <rh.hou.work@gmail.com>
2019-08-23 14:20:08 +0800

    heartrate

---
+++ app/src/main/java/org/solarex/customviewdemos/ui/HeartRateRulerActivity.java
@@ -0,0 +1,18 @@
+package org.solarex.customviewdemos.ui;
+
+import org.solarex.customviewdemos.R;
+
+/**
+ * <pre>
+ *    Author: houruhou
+ *    CreatAt: 14:11/2019-08-23
+ *    Desc:
+ * </pre>
+ */
+
+public class HeartRateRulerActivity extends BaseCustomViewActivity {
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_heartrate_ruler;
+    }
+}
--- app/src/main/java/org/solarex/customviewdemos/widgets/HeartRateRulerView.java
+++ app/src/main/java/org/solarex/customviewdemos/widgets/HeartRateRulerView.java
@@ -1,6 +1,7 @@
 package org.solarex.customviewdemos.widgets;
 
 import android.content.Context;
+import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.support.annotation.Nullable;
@@ -20,12 +21,15 @@ import java.util.ArrayList;
  */
 
 public class HeartRateRulerView extends View {
+    private static final String TAG = HeartRateRulerView.class.getSimpleName();
+
     Paint mPaint;
 
     int DEFAULT_COLOR = Color.parseColor("#DBE0E4");
-    int DEFAULT_COUNT = 200;
+    int DEFAULT_COUNT = 50;
     float DEFAULT_ITEM_WIDTH = Utils.dp2px(1);
     float DEFAULT_HIGHLIGHT_HIGHT = Utils.dp2px(9);
+    float DEFAULT_HIGHTLIGHT_MARGINTOP = Utils.dp2px(1);
     int DEFAULT_START_COLOR = Color.parseColor("#B7F27C");
     int DEFAULT_END_COLOR = Color.parseColor("#FFA5AB");
 
@@ -35,16 +39,19 @@ public class HeartRateRulerView extends View {
     int mColor = DEFAULT_COLOR;
     int mCount = DEFAULT_COUNT;
     float mItemWidth = DEFAULT_ITEM_WIDTH;
-    float mHighlightHeight = DEFAULT_HIGHLIGHT_HIGHT;
+    float mItemHeight = 2 * DEFAULT_HIGHLIGHT_HIGHT;
+    float mHighlightHeadHeight = DEFAULT_HIGHLIGHT_HIGHT;
+    float mHighlightMarginTop = DEFAULT_HIGHTLIGHT_MARGINTOP;
     int mStartColor = DEFAULT_START_COLOR;
     int mEndColor = DEFAULT_END_COLOR;
 
     int mWidth = DEFAULT_WIDTH;
     int mHeight = DEFAULT_HEIGHT;
 
-    int mHightlightIndex = -1;
+    int mHighlightIndex = -1;
     ArrayList<Integer> colors = new ArrayList<>(DEFAULT_COUNT);
-
+    float mGapWidth = 0;
+    float left,top,right,bottom;
 
     public HeartRateRulerView(Context context) {
         super(context);
@@ -59,6 +66,7 @@ public class HeartRateRulerView extends View {
     }
 
     {
+        // parse style maybe needed
         mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
         mPaint.setStyle(Paint.Style.FILL);
         mPaint.setStrokeWidth(1f);
@@ -68,10 +76,11 @@ public class HeartRateRulerView extends View {
             int color = Utils.getGradientColor(mStartColor, mEndColor, i, mCount);
             colors.add(color);
         }
+        setLayerType(View.LAYER_TYPE_SOFTWARE, null);
     }
 
     public void setProgress(float percent) {
-        mHightlightIndex = Math.round(percent * mCount);
+        mHighlightIndex = Math.round(percent * mCount);
         invalidate();
     }
 
@@ -80,5 +89,32 @@ public class HeartRateRulerView extends View {
         int width = Utils.measure(widthMeasureSpec, DEFAULT_WIDTH);
         int height = Utils.measure(heightMeasureSpec, DEFAULT_HEIGHT);
         setMeasuredDimension(width, height);
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        mWidth = w;
+        mHeight = h;
+        mGapWidth = (mWidth - mCount * mItemWidth) * 1.0f / (mCount - 1);
+        mItemHeight = mHeight - 2 * (mHighlightMarginTop + mHighlightHeadHeight);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        for (int i = 1; i <= mCount; i++) {
+            left = (i-1)*(mItemWidth+mGapWidth);
+            right = left + mItemWidth;
+            if (i == mHighlightIndex) {
+                top = mHighlightMarginTop;
+                bottom = top + mItemHeight + 2 * mHighlightHeadHeight;
+            } else {
+                top = mHighlightMarginTop + mHighlightHeadHeight;
+                bottom = top + mItemHeight;
+            }
+            mPaint.setColor(colors.get(i-1));
+            canvas.drawRect(left, top, right, bottom, mPaint);
+        }
     }
 }
