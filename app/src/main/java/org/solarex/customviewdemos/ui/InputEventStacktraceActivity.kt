53bff16077e2ceee752697a0e8855ab1a7f8c35d
Solarex <rh.hou.work@gmail.com>
2019-10-16 11:54:15 +0800

    key dispatch timeout

--- app/src/main/java/org/solarex/customviewdemos/ui/InputEventStacktraceActivity.kt
+++ app/src/main/java/org/solarex/customviewdemos/ui/InputEventStacktraceActivity.kt
@@ -1,5 +1,8 @@
 package org.solarex.customviewdemos.ui
 
+import android.os.SystemClock
+import android.util.Log
+import android.view.View
 import org.solarex.customviewdemos.R
 
 /**
@@ -8,7 +11,18 @@ import org.solarex.customviewdemos.R
  * Desc:
  */
 class InputEventStacktraceActivity : BaseCustomViewActivity() {
+    val TAG = "InputEventStacktrace"
+    var delay = 10000L;
     override fun getLayoutId(): Int {
         return R.layout.activity_input_event_stacktrace
+    }
+
+    override fun initView() {
+        val view = findViewById<View>(R.id.view_placeholder)
+        view.setOnClickListener {
+            Log.d(TAG, "current thread -> ${Thread.currentThread().name}")
+            SystemClock.sleep(delay)
+            delay+=10000
+        }
     }
 }
