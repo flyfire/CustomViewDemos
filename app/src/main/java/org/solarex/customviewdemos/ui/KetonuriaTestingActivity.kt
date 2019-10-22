fffa2e9547d7a744a8491ea3f4637a6bb8c0350f
Solarex <rh.hou.work@gmail.com>
2019-10-22 10:14:32 +0800

    custom view

--- app/src/main/java/org/solarex/customviewdemos/ui/KetonuriaTestingActivity.kt
+++ app/src/main/java/org/solarex/customviewdemos/ui/KetonuriaTestingActivity.kt
@@ -1,7 +1,9 @@
 package org.solarex.customviewdemos.ui
 
+import org.solarex.customviewdemos.R
+
 class KetonuriaTestingActivity : BaseCustomViewActivity() {
     override fun getLayoutId(): Int {
-        return super.getLayoutId()
+        return R.layout.activity_ketonuria_testing
     }
 }
