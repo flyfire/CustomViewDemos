386d7cb3061f3f1f4ad9db715e6644a8994a297c
Solarex <rh.hou.work@gmail.com>
2019-10-22 10:07:55 +0800

    custom view

---
+++ app/src/main/java/org/solarex/customviewdemos/ui/KetonuriaTestingActivity.kt
@@ -0,0 +1,7 @@
+package org.solarex.customviewdemos.ui
+
+class KetonuriaTestingActivity : BaseCustomViewActivity() {
+    override fun getLayoutId(): Int {
+        return super.getLayoutId()
+    }
+}
---
+++ app/src/main/java/org/solarex/customviewdemos/widgets/ketonuriatesting/KetonuriaTestingView.java
@@ -0,0 +1,20 @@
+package org.solarex.customviewdemos.widgets.ketonuriatesting;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class KetonuriaTestingView extends View {
+    public KetonuriaTestingView(Context context) {
+        super(context);
+    }
+
+    public KetonuriaTestingView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public KetonuriaTestingView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
