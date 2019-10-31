a6306ddea9395b4fae1d4806892d0aef48a3883a
Solarex <rh.hou.work@gmail.com>
2019-10-31 17:42:53 +0800

    touch

--- app/src/main/java/org/solarex/customviewdemos/widgets/detecttouchevent/TouchWithoutContentViewActivity.java
+++ app/src/main/java/org/solarex/customviewdemos/widgets/detecttouchevent/TouchWithoutContentViewActivity.java
@@ -1,10 +1,16 @@
 package org.solarex.customviewdemos.widgets.detecttouchevent;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.util.Log;
 import android.view.MotionEvent;
+import android.view.Window;
+
+import java.lang.reflect.Field;
 
 public class TouchWithoutContentViewActivity extends Activity {
     private static final String TAG = "TouchWithoutContentView";
@@ -12,6 +18,21 @@ public class TouchWithoutContentViewActivity extends Activity {
     public static void goTouchWithoutContentViewActivity(Context context) {
         Intent intent = new Intent(context, TouchWithoutContentViewActivity.class);
         context.startActivity(intent);
+    }
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        try {
+            @SuppressLint("PrivateApi")
+            Class clz = Class.forName("com.android.internal.policy.PhoneWindow");
+            Field mDecor = clz.getField("mDecor");
+            mDecor.setAccessible(true);
+            Object obj = mDecor.get(getWindow());
+            Log.d(TAG, "onCreate decorView = " + obj);
+        } catch (ClassNotFoundException | NoSuchFieldException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
