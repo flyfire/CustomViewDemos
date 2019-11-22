0261c47fc1ca7ffd31f43c47dac4b054ae08745a
Solarex <rh.hou.work@gmail.com>
2019-11-22 17:24:31 +0800

    avatar

---
+++ app/src/main/java/org/solarex/customviewdemos/ui/OnlineStatusAvatarActivity.kt
@@ -0,0 +1,7 @@
+package org.solarex.customviewdemos.ui
+
+class OnlineStatusAvatarActivity : BaseCustomViewActivity() {
+    override fun getLayoutId(): Int {
+        return super.getLayoutId()
+    }
+}
